46c926b7df4d7f73b2c9e5196330eccfff791de9
Ellny-design <a25ellny@student.his.se>
2025-11-21 11:11:32 +0100

    Lore Scene
    "Egen scen med kort bakgrundsstory"

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -5,6 +5,11 @@ public class Control : MonoBehaviour
 {
     public void NextScene()
     {
+        SceneManager.LoadScene("Lore");
+    }
+    
+    public void NextScene2()
+    {
         SceneManager.LoadScene("Level1");
     }
     
